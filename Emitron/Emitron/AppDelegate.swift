61559551c7520521952dfcd6f5b242395bf05c10
Lea Marolt Sonnenschein <leyamars@gmail.com>
2019-07-29 06:16:19 +0100

    Removing domains.

--- Emitron/Emitron/AppDelegate.swift
+++ Emitron/Emitron/AppDelegate.swift
@@ -40,9 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                    urlScheme: "com.razeware.emitron://",
                                    ssoSecret: AppDelegate.sso,
                                    persistentStore: persistentStore)
-  
-  let domains: [Domain] = []
-  
+    
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
