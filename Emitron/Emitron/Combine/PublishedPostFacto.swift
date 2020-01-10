c28bc52554d6644a3cc73a67d51a20af3c3fb9fe
Sam Davies <sam@razeware.com>
2020-01-10 11:30:30 +0700

    Adding a new post-facto observable object and property wrapper

---
+++ Emitron/Emitron/Combine/PublishedPostFacto.swift
@@ -0,0 +1,78 @@
+/// Copyright (c) 2020 Razeware LLC
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Combine
+
+protocol ObservablePostFactoObject: AnyObject {
+  // It's non-trivial to synthesise this, since it is a stored property. So let's not bother.
+  var objectDidChange: ObservableObjectPublisher { get }
+}
+
+
+@propertyWrapper
+struct PublishedPostFacto<Value: Equatable> {
+  
+  init(initialValue: Value) {
+    self.init(wrappedValue: initialValue)
+  }
+  
+  init(wrappedValue: Value) {
+    value = wrappedValue
+    publisher = CurrentValueSubject<Value, Never>(value)
+  }
+  
+  private var value: Value
+  private let publisher: CurrentValueSubject<Value, Never>!
+  
+  var projectedValue: AnyPublisher<Value, Never> {
+    publisher.eraseToAnyPublisher()
+  }
+  
+  var wrappedValue: Value {
+    get { fatalError("wrappedValue:get called") }
+    set { fatalError("wrappedValue:set called") }
+  }
+  
+  static subscript<EnclosingSelf: ObservablePostFactoObject>(
+  _enclosingInstance observablePostFacto: EnclosingSelf,
+  wrapped wrappedKeyPath: ReferenceWritableKeyPath<EnclosingSelf, Value>,
+  storage storageKeyPath: ReferenceWritableKeyPath<EnclosingSelf, Self>
+  ) -> Value {
+    get {
+      observablePostFacto[keyPath: storageKeyPath].value
+    }
+    set {
+      if observablePostFacto[keyPath: storageKeyPath].value != newValue {
+        observablePostFacto[keyPath: storageKeyPath].value = newValue
+        observablePostFacto[keyPath: storageKeyPath].publisher.send(newValue)
+        observablePostFacto.objectDidChange.send()
+      }
+    }
+  }
+}
+
