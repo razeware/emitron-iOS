cf77f89d765d70207d5189817075ec446049cbe6
Franklin Byaruhanga <byaruhaf@appbantu.com>
2020-11-13 16:53:33 +0200

    Change hasher to use "display" to match the rest settings.

--- Emitron/Emitron/Models/PlaybackSpeed.swift
+++ Emitron/Emitron/Models/PlaybackSpeed.swift
@@ -70,6 +70,6 @@ enum PlaybackSpeed: Int, CaseIterable, SettingsSelectable {
 extension PlaybackSpeed {
   // Added hash(into: ) funciton because of this bug: https://bugs.swift.org/browse/SR-13851
   func hash(into hasher: inout Hasher) {
-    hasher.combine(rate)
+    hasher.combine(display)
   }
 }
