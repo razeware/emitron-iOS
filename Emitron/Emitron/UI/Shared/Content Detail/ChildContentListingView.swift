18673ba993830ec98ebb569e2623d24954c36f70
Brian Moakley <brian.douglas.moakley@gmail.com>
2021-04-30 18:12:51 -0400

    Removes close button

--- Emitron/Emitron/UI/Shared/Content Detail/ChildContentListingView.swift
+++ Emitron/Emitron/UI/Shared/Content Detail/ChildContentListingView.swift
@@ -62,11 +62,6 @@ private extension ChildContentListingView {
               .kerning(-0.5)
               .foregroundColor(.titleText)
               .padding([.top, .bottom])
-            Button {
-              exit(0)
-            } label: {
-              Text("Close App0")
-            }
             Spacer()
           }.padding([.leading, .trailing], 20)
         }
