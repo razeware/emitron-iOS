0f0b5c62bb878f562d004f1aaa4cb2b1541914e6
Jessy Catterwaul <jessy@razeware.com>
2022-02-05 02:50:59 -0500

    Undo "Fix bar appearance in iOS 15"
    Originally commited as a80f1f2b376bc97c8e2b6442042b27fbcc3466aa. Apple has changed rendering since then.

--- Emitron/Emitron/UI/App Root/TabNavView.swift
+++ Emitron/Emitron/UI/App Root/TabNavView.swift
@@ -44,19 +44,6 @@ struct TabNavView<
     self.myTutorialsView = myTutorialsView
     self.downloadsView = downloadsView
     self.settingsView = settingsView
-
-    // Without the following, in iOS 15, this ugliness occurs:
-    //
-    // The nav bar renders as a rectangle that does not extend vertically past the safe area.
-    // Scrolling content is visible above it.
-    //
-    // The tab bar renders transparently, on top of other views.
-    if #available(iOS 15.0, *) {
-      let barAppearance = UIBarAppearance()
-      barAppearance.configureWithOpaqueBackground()
-      UITabBar.appearance().scrollEdgeAppearance = .init(barAppearance: barAppearance)
-      UINavigationBar.appearance().scrollEdgeAppearance = .init(barAppearance: barAppearance)
-    }
   }
 
   @EnvironmentObject private var model: TabViewModel
