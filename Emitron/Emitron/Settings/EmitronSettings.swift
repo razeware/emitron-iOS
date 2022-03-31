f7f05546487efc7acd80315d8e6132b711dbc372
Jessy Catterwaul <jessy@razeware.com>
2022-03-31 14:29:49 -0400

    Cleanup

--- Emitron/Emitron/Settings/EmitronSettings.swift
+++ Emitron/Emitron/Settings/EmitronSettings.swift
@@ -26,7 +26,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import Combine
+import struct Combine.AnyPublisher
 
 protocol EmitronSettings {
   // MARK: - Library
